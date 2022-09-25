e69e1d155392eef05c2c3205df70c1a9aa1b588d
Emil Gafarov <113677882+gafarovExe@users.noreply.github.com>
2022-09-25 19:54:06 +0300

    Update level3task7.cs
    The end of the end

--- level3task7.cs
+++ level3task7.cs
@@ -11,37 +11,34 @@ class HelloWorld
         double factorial = 1;
         double d = 0;
         double proverka = 0;
-        double t=1;
+        double t = 1;
         double rad = 0;
         double uravoty = 0;
         double tochnostfuncii = 0;
-         for (double x = 0.1; x <= 1.05; x = x + 0.05)
-         {
+        for (double x = 0.1; x <= 1.05; x = x + 0.05)
+        {
             //double x = 0.2;
             proverka = 0;
             for (double i = 0; proverka == 0; i++)
             {
                 d = 1;
-                factorial = factorial * i;
+                factorial = factorial * 2*i * (2*i-1);
                 if (i == 0) factorial = 1;
-                if (i % 2 == 0 || i == 0)
-                {
-                    d =  Math.Pow(x, i) / factorial;
+                    d = Math.Pow(x, 2*i) / factorial;
                     sum = sum + d;
                     if (d < 0) { d = d * -1; }
                     if (d < 0.0001) { proverka = 1; }
                     //Console.WriteLine("1");
-                }
             }
             //rad = x * 0.0175; //перевод в радианы
             uravoty = (Math.Pow(Math.E, x) + Math.Pow(Math.E, -x)) / 2;
             tochnostfuncii = uravoty - sum;
             Console.WriteLine("Summ " + t + " = " + sum);
             Console.WriteLine("Accuracy of function " + t + " = " + tochnostfuncii);
-            Console.WriteLine(x);
+            //Console.WriteLine(x);
             t = t + 1;
             sum = 0;
-         }
+        }
         return 0;
     }
 }
